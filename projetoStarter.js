a42cd7235e1d248de81092cfd62e63c50d455c6e
Flávia Reis <flaviarster@gmail.com>
2024-02-06 20:16:24 -0300

    Criei mais arquivos

--- projetoStarter.js
+++ projetoStarter.js
@@ -12,18 +12,37 @@ diretorios.forEach(diretorio => {
       })
 })
 
-fs.writeFileSync('index.html', `
+
+fs.writeFileSync('./meuprojeto/index.html', `
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./meuprojeto/assets/css/style.css">
     <title>Document</title>
 </head>
 <body>
     
 </body>
+<script src="meuprojeto/assets/js/script.js"></script>
 </html>`)
+
+fs.writeFileSync('./meuprojeto/assets/js/script.js', 'alert("Deu certo!")')
+
+fs.writeFileSync('./meuprojeto/assets/css/style.css', `
+* {
+    margin: '0px';
+    padding: '0px';
+}
+`)
+
+
+
+
+
+
+
 
 
 
